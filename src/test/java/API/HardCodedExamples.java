dc7d73e57426f8ca2d7b05f322b9b9b487c83883
Hİlmi <hlmozcelik1@gmail.com>
2022-06-16 14:37:26 +0300

    cucumber with sql cucumber with API cucumber with API2

--- src/test/java/API/HardCodedExamples.java
+++ src/test/java/API/HardCodedExamples.java
@@ -6,12 +6,13 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 
 public class HardCodedExamples {
    String baseURI=RestAssured.baseURI="http://hrm.syntaxtechs.net/syntaxapi/api";
-   String token="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NTUzMjcxODcsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTY1NTM3MDM4NywidXNlcklkIjoiMzc5NCJ9.HGunfoxahGC4sGuFnFIWsMnSDU1HgwR1v36vKcwOXGA";
-
+   String token="Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2NTUzNzc5MTEsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTY1NTQyMTExMSwidXNlcklkIjoiMzc5NCJ9.aSZJXoZ_R1kg_w-Yzkm7RYdNo3icQj-PCqH9YHNOnRE";
+   static String employee_id;
    @Test
    public void createEmployee(){
       RequestSpecification request= given().header("Content-Type","application/json").
@@ -26,5 +27,15 @@ public class HardCodedExamples {
                       "}");
      Response response= request.when().post("/createEmployee.php"); //postmandan gelen en point swaggerde post oldugu için
       response.prettyPrint();
+       response.then().assertThat().statusCode(201);
+       //Hamcrest matchers=> to create matcher objects
+       //prettyPrint => is used for printing response to console
+       response.then().assertThat().body("Message",equalTo("Employee Created"));
+    //bu şekilde null value verir//response.then().assertThat().body("emp_firstname",equalTo( "Hilmi"));
+       response.then().assertThat().body("Employee.emp_firstname",equalTo( "Hilmi"));
+       // ardına key e belirlemek için jsonPath metodu kullanılmalı
+      employee_id= response.jsonPath().getString("Employee.employee_id");
+       System.out.println(employee_id);
    }
+
 }
